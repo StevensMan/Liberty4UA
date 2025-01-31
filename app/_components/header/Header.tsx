e7f4538cc096cb92587c37703a72dd0a73d7cb77
r0leksii <rozhkoaleks@gmail.com>
2025-01-31 09:44:52 +0200

    Add dynamic header background and active menu state

--- app/_components/header/Header.tsx
+++ app/_components/header/Header.tsx
@@ -1,12 +1,37 @@
-// import Image from "next/image";
+'use client'
+
+import Image from 'next/image'
 import { Container, MainMenu } from '@/app/_components'
+import { usePathname } from 'next/navigation'
 import Link from 'next/link'
+import { routes } from '@/lib/routes'
+import { cn } from '@/lib/utils'
 
 export const Header = () => {
+  const pathname = usePathname()
+
+  const isHome = pathname === routes.home
+
   return (
-    <header className="relative z-50 h-[100px] w-full bg-transparent">
+    <header
+      className={cn(
+        'relative z-50 h-[100px] w-full',
+        isHome && 'bg-transparent'
+      )}
+    >
+      {!isHome && (
+        <>
+          <Image
+            src="/header-bg-1.jpg"
+            alt="Liberty4UA"
+            fill
+            className="absolute left-0 top-0"
+          />
+          <div className="absolute inset-0 bg-black opacity-30"></div>
+        </>
+      )}
       <Container className="relative flex h-full items-center justify-between">
-        <Link href="/">
+        <Link href={routes.home}>
           <p className="text-2xl font-bold text-white">
             Liberty4<span className="text-yellow-400">UA</span>
           </p>
--- app/_components/main-menu/MainMenu.tsx
+++ app/_components/main-menu/MainMenu.tsx
@@ -1,21 +1,34 @@
+'use client'
+
 import Link from 'next/link'
 import { routes } from '@/lib/routes'
+import { usePathname } from 'next/navigation'
+import { cn } from '@/lib/utils'
 
 export const MainMenu = () => {
+  const pathname = usePathname()
+
   return (
     <ul className="flex flex-row items-center justify-center gap-8 text-white">
       <li>
-        <Link className="hover:text-yellow-400" href={routes.home}>
-          Home
-        </Link>
-      </li>
-      <li>
-        <Link className="hover:text-yellow-400" href={routes.about}>
+        <Link
+          className={cn(
+            'hover:text-yellow-400',
+            pathname === routes.about && 'text-yellow-400'
+          )}
+          href={routes.about}
+        >
           About
         </Link>
       </li>
       <li>
-        <Link className="hover:text-yellow-400" href={routes.contact}>
+        <Link
+          className={cn(
+            'hover:text-yellow-400',
+            pathname === routes.contact && 'text-yellow-400'
+          )}
+          href={routes.contact}
+        >
           Contact
         </Link>
       </li>
